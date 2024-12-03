bb3b80fe9a3d140e80bdde21a1a94e45edd2cd71
longedemagnata <borbaeric87@gmail.com>
2024-12-03 11:32:31 -0300

    Add files via upload

---
+++ aula02/aula02.py
@@ -0,0 +1,12 @@
+print("olá,mundo")
+print("Amo,minha mulher")
+nome_usuario = "eric "# tipo-> str = string]
+idade_usuario = 17 #-> int = integer
+altura_usuario = 1.69
+maior_idade = True # tipo -> bool = boolean
+
+print("nome_usuario")
+
+print("seja bem-vindo", nome_usuario)
+print("sua idadade é", idade_usuario"anos)
+print('voce tem',)
